f89ac7f61feda542dd95e98f8ca4ed2e87d80bae
Karel De Smet <karel@Karel.be>
2018-06-15 10:26:51 +0200

    stable version v1.7.2

--- Space Breakout/bricks.py
+++ Space Breakout/bricks.py
@@ -21,48 +21,15 @@ def main():
             #backgrouns scrolling
             setDynamicBackground()
             #levens
-            for i in range(gb.lives): #3 levens = 0,1,2
-                x,y = ((gb.heartRect[2]*i)+5,5)
-                gb.mainSurface.blit(gb.heartSprite,(x,y))
-            #onscreen text
-            scoreLabel = gb.fontobjTITEL.render(str(gb.score),True,gb.white,None)
-            scoreComboLabel = gb.fontobjCOMBO.render("combo!  "+ str(gb.scoreTemp),True,gb.white,None)
-            LevelindicatorLabel = gb.fontobj.render("Level:"+str(gb.level),True,gb.white,None)
-            gb.mainSurface.blit(scoreLabel,(gb.WIDTH-scoreLabel.get_width()-5,5))
-            if gb.DEVELOPER_TOOLS and gb.showCheatKeys:
-                drawMultipleLines(gb.cheatStringList,gb.white)
-            if gb.scoreTemp > 4 and gb.changeBall:
-                gb.mainSurface.blit(scoreComboLabel,(400-int(scoreComboLabel.get_width()/2),6))
-            if gb.scoreTemp > 2 and not gb.changeBall:
-                gb.mainSurface.blit(scoreComboLabel,(400-int(scoreComboLabel.get_width()/2),6))
-            gb.mainSurface.blit(LevelindicatorLabel,(0,gb.HEIGHT-LevelindicatorLabel.get_height()-10))
+            drawHUD()
             #events
-            for event in pygame.event.get():
-                checkKeyQuit(event)
-                if event.type == pygame.MOUSEBUTTONUP:
-                    if not gb.ballServed:
-                        gb.ballServed = True
-                elif event.type == pygame.MOUSEMOTION:
-                    checkMouseEvents(event)
-                elif event.type == pygame.KEYDOWN:
-                    checkKeyDownEvents(event)
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT and gb.keyDown == "K_LEFT":
-                        gb.keyDown = None
-                    elif event.key == pygame.K_RIGHT and gb.keyDown == "K_RIGHT":
-                        gb.keyDown = None
-            if gb.keyDown:
-                checkKeyHoldingEvents(gb.keyDown)
+            checkLevelEvents()
             if gb.changeBat:
                 gb.mainSurface.blit(gb.batLangSprite, gb.batLangRect)
             if not gb.changeBat:
                 gb.mainSurface.blit(gb.batSprite, gb.batRect)
-            # teken stenen
-            tekenStenen()
-            #teken upgrades
-            for u in gb.upgradeRectList:
-                #collision detection
-                checkBallUpgradeCollide(u)
+            drawBricks()
+            drawUpgrades()
             # teken pallet en bal
             if gb.ballServed:
                 gb.bx -= gb.sx
@@ -122,6 +89,9 @@ def main():
         while gb.gameOverMenu:
             showGameOverMenu()
             pygame.display.update()
+#######################################################################################################
+#Shows the Game Over Screen.
+#includes "Eindscore" & press space to play again
 def showGameOverMenu():
     eindScore = gb.fontobjTITEL.render("Eindscore: " + str(gb.score), True, gb.white, None)
     gb.mainSurface.blit(gb.gameOverBg,(0,0))
@@ -136,6 +106,10 @@ def showGameOverMenu():
                 gb.gameOn = False
                 pygame.quit()
                 sys.exit()
+
+
+#Shows the "Completed level" screen
+#Ability to setup all variables needed in setupNextLevel()
 def showLevelChange():
     relatief_X = gb.xBg % gb.mainSurface.get_rect().height
     gb.mainSurface.blit(gb.nextLevelBg,(relatief_X - gb.mainSurface.get_rect().width,0))
@@ -154,6 +128,10 @@ def showLevelChange():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 setupNextLevel()
+
+
+#Shows the Menu screen
+#This screen only shown in beginning, ability to show instructions to the player
 def showMenu():
     for event in pygame.event.get():
         checkKeyQuit(event)
@@ -164,13 +142,18 @@ def showMenu():
     gb.mainSurface.blit(gb.bgMain,(0,0))
     gb.mainSurface.blit(gb.welkomLabel,(400-int(gb.welkomLabel.get_width()/2),50))
     drawMultipleLines(gb.stringMenuList,gb.white,gb.welkomLabel.get_width(),300)
-def createBricks(specials1PerLevel,specials2PerLevel,sleutels, lvl = gb.level,height = gb.HEIGHT, width = gb.WIDTH):
-    rands = specials1PerLevel*lvl
-    rands2 = specials2PerLevel*lvl
+
+
+#generates the bricksets in levels
+#Returns 1 list of all bricks with ID's and 1 list with only Rectangles
+#Ability to randomise
+def createBricks(specials1PerLevel,specials2PerLevel,sleutels,height = gb.HEIGHT, width = gb.WIDTH):
+    rands = specials1PerLevel*gb.level
+    rands2 = specials2PerLevel*gb.level
     rands3 = sleutels
     bricksTemp,bricksRectsTemp,randomIndex1,randomIndex2,randomIndex3 = [],[],[],[],[]
-    YrangeVoorX,YrangeVoorY = 4+lvl,8+lvl
-    XrangeVoorX,XrangeVoorY = 5+lvl,9+lvl
+    YrangeVoorX,YrangeVoorY = 4+gb.level,8+gb.level
+    XrangeVoorX,XrangeVoorY = 5+gb.level,9+gb.level
     if(YrangeVoorY >= 20):
         YrangeVoorX,YrangeVoorY = 13,20
     if (XrangeVoorY >= 16):
@@ -187,10 +170,10 @@ def createBricks(specials1PerLevel,specials2PerLevel,sleutels, lvl = gb.level,he
         brickY = (y * 16) -100 + ((height-(yRange*16))/2)
         for x in range(xRange):
             brickX = (x*48) + ((width-(xRange*48))/2)
-            if lvl >= 10:
+            if gb.level >= 10:
                 tekenkansY = r.randrange(0,2)
             else:
-                tekenkansY = r.randrange(0,12-lvl)
+                tekenkansY = r.randrange(0,12-gb.level)
             if tekenkansY != 0 or (x,y) in randomIndex3: #100% kans voor 2 sleutels
                 if (x,y) in randomIndex3:
                     bricksTemp.append((Rect(brickX,brickY,48,16),3)) #brick_sleutel
@@ -202,7 +185,11 @@ def createBricks(specials1PerLevel,specials2PerLevel,sleutels, lvl = gb.level,he
                     bricksTemp.append((Rect(brickX,brickY,48,16),0)) #brick
                 bricksRectsTemp.append(Rect(brickX,brickY,48,16))
     return bricksRectsTemp,bricksTemp
-def showCheatMenu(event):
+
+
+#Shows and Handles the Cheat menu
+#ability to cheat using keyhandles & set show or hide cheat menu
+def handleCheatMenu(event):
     if event.key == pygame.K_RETURN:
         if gb.showCheatKeys:
             gb.showCheatKeys = False
@@ -229,6 +216,30 @@ def showCheatMenu(event):
                 gb.sx,gb.sy = (gb.ballSpeed,gb.ballSpeed)
             elif gb.sx > 0 and gb.sy > 0:
                 gb.sx,gb.sy = (gb.ballSpeed,gb.ballSpeed)
+
+
+#draws the Head-Up Display
+#Hud includes: Lives, score, comboscore and levelindicator
+def drawHUD():
+    for i in range(gb.lives): #3 levens = 0,1,2
+        x,y = ((gb.heartRect[2]*i)+5,5)
+        gb.mainSurface.blit(gb.heartSprite,(x,y))
+    #onscreen text
+    scoreLabel = gb.fontobjTITEL.render(str(gb.score),True,gb.white,None)
+    scoreComboLabel = gb.fontobjCOMBO.render("combo!  "+ str(gb.scoreTemp),True,gb.white,None)
+    LevelindicatorLabel = gb.fontobj.render("Level:"+str(gb.level),True,gb.white,None)
+    gb.mainSurface.blit(scoreLabel,(gb.WIDTH-scoreLabel.get_width()-5,5))
+    if gb.DEVELOPER_TOOLS and gb.showCheatKeys:
+        drawMultipleLines(gb.cheatStringList,gb.white)
+    if gb.scoreTemp > 4 and gb.changeBall:
+        gb.mainSurface.blit(scoreComboLabel,(400-int(scoreComboLabel.get_width()/2),6))
+    if gb.scoreTemp > 2 and not gb.changeBall:
+        gb.mainSurface.blit(scoreComboLabel,(400-int(scoreComboLabel.get_width()/2),6))
+    gb.mainSurface.blit(LevelindicatorLabel,(0,gb.HEIGHT-LevelindicatorLabel.get_height()-10))
+
+
+#sets up all variables for a New Game
+#ability to create a new brickSet for the next New game
 def resetForNewGame():
     gb.gameOverMenu = False
     gb.levelsPlaying = True
@@ -242,11 +253,19 @@ def resetForNewGame():
     gb.level = 1
     gb.keyDown = ""
     gb.bricksRects,gb.bricks = createBricks(4,2,2)
+
+
+#Create x amount of randoms between 0 and 9
+#returns them in a list
 def createRandoms(randoms):
     rands = []
     for i in range(randoms):
         rands.append(r.randrange(1,10))
     return rands
+
+
+#checks all mouseEvents
+#ability to change the bat, with ball served or not
 def checkMouseEvents(event):
     gb.mouseX = event.pos[0] #mousex = X positie van de muis
     if(gb.mouseX < gb.WIDTH or gb.mouseX <= 0 ):#
@@ -269,8 +288,15 @@ def checkMouseEvents(event):
             elif not gb.changeBall:
                 gb.bx,gb.by = (gb.mouseX-int(gb.ballRect[2]/2),gb.playerY-gb.batRect[3])
             gb.ballRect.topleft = (gb.bx,gb.by)
+
+
+#Dictionary with backgrouds
 def bg(x):
     return ((gb.bg1,gb.bg2,gb.bg3,gb.bg4)[x])
+
+
+#Sets and draws the dynamicBackgroud frame
+#background slides down endlessly
 def setDynamicBackground():
     lvl = gb.level
     background = bg(lvl%4)
@@ -280,6 +306,10 @@ def setDynamicBackground():
     if relatief_Y < gb.HEIGHT:
         gb.mainSurface.blit(background, (0,relatief_Y))
     gb.yBg += 1
+
+
+#sets up and resets all variables
+#ability to speed up the ballspeed and create new list
 def setupNextLevel():
     gb.keyDown = ""
     gb.scoreTemp = 0
@@ -297,37 +327,59 @@ def setupNextLevel():
     gb.ballRect.topleft = (gb.bx,gb.by) = ((gb.WIDTH/2)-int(gb.ballRect[2]/2),gb.playerY-gb.ballRect[3])
     gb.batRect.topleft = gb.batLangRect.topleft = ((gb.WIDTH/2)-int(gb.batRect[2]/2),gb.playerY)
     gb.bricksRects,gb.bricks = createBricks(4,2,2)
+
+#Definition to draw multiple lines at once
+#using a list of strings, draws lines with offsets
 def drawMultipleLines(stringArray,color = (0,0,0),  widthOffset = 0, heightOffset = 0,bgcolor = None,AA = True):
     count = 0
     for string in stringArray:
         stringLabel = gb.fontCheatKeys.render(string,AA,color,bgcolor)
         gb.mainSurface.blit(stringLabel,(gb.WIDTH-stringLabel.get_width() - widthOffset,(gb.HEIGHT-stringLabel.get_height()*count) - heightOffset ))
         count += 1
-def tekenStenen():
+
+
+#draws all bricks in the list
+#using the ID draws the correct brick
+def drawBricks():
     for c in gb.bricksRects:
-        status = ""
-        index = None
         for b in gb.bricks:
             if b[-1] == 0 and b[-2]==c:
-                status = ""
+                index = gb.bricks.index((c,0))
+                gb.mainSurface.blit(gb.brick,gb.bricks[index][-2])
             elif b[-1] == 1 and b[-2]==c:
-                status = "upgradeBlauw"
+                index = gb.bricks.index((c,1))
+                gb.mainSurface.blit(gb.brickBlauw,gb.bricks[index][-2])
             elif b[-1] == 2 and b[-2]==c:
-                status = "upgradeGeel"
+                index = gb.bricks.index((c,2))
+                gb.mainSurface.blit(gb.brickGeel,gb.bricks[index][-2])
             elif b[-1] == 3 and b[-2]==c:
-                status = "upgradeSleutel"
-        if status == "":
-            index = gb.bricks.index((c,0))
-            gb.mainSurface.blit(gb.brick,gb.bricks[index][-2])
-        elif status == "upgradeBlauw":
-            index = gb.bricks.index((c,1))
-            gb.mainSurface.blit(gb.brickBlauw,gb.bricks[index][-2])
-        elif status == "upgradeGeel":
-            index = gb.bricks.index((c,2))
-            gb.mainSurface.blit(gb.brickGeel,gb.bricks[index][-2])
-        elif status == "upgradeSleutel":
-            index = gb.bricks.index((c,3))
-            gb.mainSurface.blit(gb.brickSleutel,gb.bricks[index][-2])
+                index = gb.bricks.index((c,3))
+                gb.mainSurface.blit(gb.brickSleutel,gb.bricks[index][-2])
+
+
+#main checker for ALL events in the level
+#includes mouse, keypresses and keyholds
+def checkLevelEvents():
+    for event in pygame.event.get():
+        checkKeyQuit(event)
+        if event.type == pygame.MOUSEBUTTONUP:
+            if not gb.ballServed:
+                gb.ballServed = True
+        elif event.type == pygame.MOUSEMOTION:
+            checkMouseEvents(event)
+        elif event.type == pygame.KEYDOWN:
+            checkKeyDownEvents(event)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and gb.keyDown == "K_LEFT":
+                gb.keyDown = None
+            elif event.key == pygame.K_RIGHT and gb.keyDown == "K_RIGHT":
+                gb.keyDown = None
+    if gb.keyDown:
+        checkKeyHoldingEvents(gb.keyDown)
+
+
+#checks all keydownevents
+#includes Left,Right and Space
 def checkKeyDownEvents(event ):
     if event.key == pygame.K_LEFT:
         gb.keyDown = "K_LEFT"
@@ -336,15 +388,23 @@ def checkKeyDownEvents(event ):
         gb.keyDown = "K_RIGHT"
         doKeyRight()
     if(gb.DEVELOPER_TOOLS):
-        showCheatMenu(event)
+        handleCheatMenu(event)
     if event.key == pygame.K_SPACE:
         if not gb.ballServed:
             gb.ballServed = True
+
+
+#checks all keyholding events
+#includes left and right
 def checkKeyHoldingEvents(keyDown):
     if keyDown == "K_LEFT":
         doKeyLeft()
     if keyDown == "K_RIGHT":
         doKeyRight()
+
+
+#sets the location of bat if ball not served
+#Use KEYBOARD_SPEED to change the speed of the bat
 def doKeyLeft():
     if gb.changeBat and gb.batLangRect[0] > 0:
         gb.batLangRect.topleft = (gb.batLangRect[0]-gb.KEYBOARD_SPEED,gb.playerY)
@@ -353,6 +413,10 @@ def doKeyLeft():
     if not gb.ballServed:
         gb.bx,gb.by = setLocationUnservedBall(gb.changeBat)
         gb.ballRect.topleft = (gb.bx,gb.by)
+
+
+#sets the location of the bat if balll not served
+
 def doKeyRight():
     if gb.changeBat and gb.batLangRect[0] < gb.WIDTH-gb.batLangRect[2]:
         gb.batLangRect.topleft = (gb.batLangRect[0]+gb.KEYBOARD_SPEED,gb.playerY)
@@ -361,11 +425,33 @@ def doKeyRight():
     if not gb.ballServed:
         gb.bx,gb.by = setLocationUnservedBall(gb.changeBat)
         gb.ballRect.topleft = (gb.bx,gb.by)
+
+
+#sets the location of the bat with ball on top
 def setLocationUnservedBall(changeBat):
     if changeBat:
         return (gb.batLangRect[0]+int(gb.batLangRect[2]/2)-int(gb.ballRect[2]/2),gb.playerY-gb.batLangRect[3])
     else:
         return (gb.batRect[0]+int(gb.batRect[2]/2)-int(gb.ballRect[2]/2),gb.playerY-gb.batRect[3])
+
+
+#draws all upgrades according to the ID
+#also goes to check if current upgrades is not collided
+def drawUpgrades():
+    for upgrade in gb.upgradeRectList:
+        #collision detection
+        if(upgrade[-1]==1):
+            gb.mainSurface.blit(gb.upgradeBlauw,(upgrade[-2].topleft))
+        elif(upgrade[-1]==2):
+            gb.mainSurface.blit(gb.upgradeGeel,(upgrade[-2].topleft))
+        elif(upgrade[-1]==3):
+            gb.mainSurface.blit(gb.upgradeSleutel,(upgrade[-2].topleft))
+        upgrade[-2].topleft = (upgrade[-2][0],upgrade[-2][1]+gb.ballSpeed-2) #upgrades naar beneden laten vallen, speed = 2
+        checkBallUpgradeCollide(upgrade)
+
+
+#check for the ball collides
+#ability to check for up, down, left and right of the screen
 def checkBallCollides():
     #Onderkant
     if(gb.by <= 0):
@@ -396,6 +482,10 @@ def checkBallCollides():
     if(gb.bx >= gb.WIDTH):
         gb.sx *= -1
         gb.bx = gb.WIDTH
+
+
+#check if the ball collide with bat
+#if collides then change bat and/or ball to normal & play sound
 def checkBallBatCollide():
     collide = False
     if not gb.changeBall and ((gb.ballRect.colliderect(gb.batRect) and not gb.changeBat)or(gb.ballRect.colliderect(gb.batLangRect) and gb.changeBat)):
@@ -418,6 +508,10 @@ def checkBallBatCollide():
         if gb.changeBat:
             gb.batRect.topleft = gb.batLangRect.topleft
             gb.changeBat = False
+
+
+#checks if the upgrades collided with ball
+#goes to deletes the nessecary upgrade and upgrades the player
 def checkBallUpgradeCollide(upgrade):
     if(upgrade[-1]==1):
         gb.mainSurface.blit(gb.upgradeBlauw,(upgrade[-2].topleft))
@@ -426,33 +520,41 @@ def checkBallUpgradeCollide(upgrade):
     elif(upgrade[-1]==3):
         gb.mainSurface.blit(gb.upgradeSleutel,(upgrade[-2].topleft))
     upgrade[-2].topleft = (upgrade[-2][0],upgrade[-2][1]+gb.ballSpeed-2) #upgrades naar beneden laten vallen, speed = 2
-    if gb.changeBat:
+    if gb.batLangRect.colliderect(upgrade[-2]):
         deleteUpgradeByRect(upgrade)
-        if(gb.batLangRect.colliderect(upgrade[-2]) and upgrade[-1]==1):
+        if upgrade[-1]==1:
             gb.changeBall = True
-        elif(gb.batLangRect.colliderect(upgrade[-2]) and upgrade[-1]==2):
+        elif upgrade[-1]==2:
             gb.batLangRect.topleft = gb.batRect.topleft
-    elif not gb.changeBat:
+    elif gb.batRect.colliderect(upgrade[-2]):
         deleteUpgradeByRect(upgrade)
-        if(gb.batRect.colliderect(upgrade[-2]) and upgrade[-1]==1):
+        if upgrade[-1]==1:
             gb.changeBall = True
-        elif(gb.batRect.colliderect(upgrade[-2]) and upgrade[-1]==2):
+        elif upgrade[-1]==2:
             gb.batLangRect.topleft = gb.batRect.topleft
             gb.changeBat = True
-    if(gb.batRect.colliderect(upgrade[-2]) and upgrade[-1]==3) or (gb.batLangRect.colliderect(upgrade[-2]) and upgrade[-1]==3):
+    elif(upgrade[-2][1] >= gb.HEIGHT-8):
         deleteUpgradeByRect(upgrade)
+    if(gb.batRect.colliderect(upgrade[-2]) and upgrade[-1]==3) or (gb.batLangRect.colliderect(upgrade[-2]) and upgrade[-1]==3):
         gb.changeBat = False
         gb.levelsPlaying = False
         gb.changeLevel = True
         gb.score += gb.scoreTemp
     #out of bound detection
-    if(upgrade[-2][1] >= gb.HEIGHT-8):
-        deleteUpgradeByRect(upgrade)
+
+
+#easy delete of the upgrade using the object
 def deleteUpgradeByRect(upgrade):
     del(gb.upgradeRectList[gb.upgradeRectList.index(upgrade)])
+
+
+#check for the quit event in event.type
 def checkKeyQuit(event):
     if event.type == pygame.QUIT:
         pygame.quit()
         sys.exit()
+
+
+#do main as last
 if __name__ == '__main__':
     main()
